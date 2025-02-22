4022413e610206e722643c0945b2bf5cfd819f1e
Wutthipat Wutthipattharatham <50732598+wutthibook@users.noreply.github.com>
2025-02-23 01:03:32 +0900

    more bugs fix

--- sum_numbers_3.c
+++ sum_numbers_3.c
@@ -82,7 +82,7 @@ int main() {
 
     // Print the sum of the numbers
     printf("1st: %.2f 2nd: %.2f 3rd: %.2f 4th: %.2f\n", sum1/100, sum2/100, sum3/100);
-    sum = sum1+sum2+sum3+sum4;
+    sum = sum1+sum2+sum3;
     printf("The sum of all numbers in the file is: %.2f\n", sum/100);
 
     return 0;
